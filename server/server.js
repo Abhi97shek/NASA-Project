a265d93e7e95a6981dad576f7c5dcdb5dc89ecce
Abhi97shek <abhi.kumar736@gmail.com>
2023-03-13 22:59:54 +0530

    Frontend and Backend Run on Same Server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app =express();
+const path = require('path');
 const PORT =8000;
 const planetRouter = require('./routes/planets/planets.routes');
 
 
 
 app.use(express.json());
-
-
+app.use(express.static(path.join(__dirname,'public')))
 app.use(planetRouter);
 
 app.listen(PORT,()=>{
